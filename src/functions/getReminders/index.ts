bb2413900a825e8c2a061bc183d2b6b67f32314b
Ryan Kirsch <agalloch88@gmail.com>
2023-01-27 14:50:22 -0500

    add logic for getReminders

--- src/functions/getReminders/index.ts
+++ src/functions/getReminders/index.ts
@@ -5,11 +5,25 @@ import { dynamo } from "@libs/dynamo";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   try {
-    
+    const tableName = process.env.reminderTable;
 
-    return formatJSONResponse({ data: {
-     
-    } });
+    const { userId } = event.pathParameters || {};
+
+    if (!userId) {
+      return formatJSONResponse({
+        statusCode: 400,
+        data: {
+          message: "userId is required",
+        },
+      });
+    }
+    const data = await dynamo.query({
+      tableName,
+      index: "index1",
+      pkValue: userId,
+    });
+
+    return formatJSONResponse({ data });
   } catch (error) {
     console.log(error);
     return formatJSONResponse({
